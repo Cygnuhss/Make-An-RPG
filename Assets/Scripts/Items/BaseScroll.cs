c00bf418a9d8b8d5db9e296f9c6fc7c43b8d9721
Cygnuhss <jelmervannuss@yahoo.com>
2015-04-03 10:05:13 +0200

    Make An RPG Episode 4: Item Creation Part 2
    Added Equipment, Potion and Scroll items.

---
+++ Assets/Scripts/Items/BaseScroll.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseScroll : BaseStatItem {
+
+	private int spellEffectID;
+
+	public int SpellEffectID {
+		get{return spellEffectID;}
+		set{spellEffectID = value;}
+	}
+}
