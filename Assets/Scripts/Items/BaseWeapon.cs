73b35895817737b2259f5512bfd13e32650bb7e6
Cygnuhss <jelmervannuss@yahoo.com>
2015-04-03 09:50:38 +0200

    Make An RPG Episode 3: Item Creation Part 1
    Added Base, Stat and Weapon items.

---
+++ Assets/Scripts/Items/BaseWeapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWeapon : BaseStatItem {
+	
+	public enum WeaponTypes {
+		SWORD,
+		STAFF,
+		DAGGER,
+		BOW,
+		SHIELD,
+		POLEARM
+	}
+	private WeaponTypes weaponType;
+	private int spellEffectID;
+
+	public WeaponTypes WeaponType {
+		get{return weaponType;}
+		set{weaponType = value;}
+	}
+
+	public int SpellEffectID {
+		get{return spellEffectID;}
+		set{spellEffectID = value;}
+	}
+}
