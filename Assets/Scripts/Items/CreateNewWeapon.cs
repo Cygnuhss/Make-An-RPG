959db22fc83a1246e93b1313f53450448a191b22
Cygnuhss <jelmervannuss@yahoo.com>
2015-04-03 10:22:06 +0200

    Make An RPG Episode 5: Item Creation Part 3
    Added weapon creation.

---
+++ Assets/Scripts/Items/CreateNewWeapon.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateNewWeapon : MonoBehaviour {
+
+	private BaseWeapon newWeapon;
+
+	void Start() {
+		CreateWeapon ();
+		Debug.Log(newWeapon.ItemName);
+		Debug.Log(newWeapon.ItemDescription);
+		Debug.Log(newWeapon.ItemID.ToString ());
+		Debug.Log(newWeapon.WeaponType.ToString ());
+		Debug.Log(newWeapon.Stamina.ToString ());
+		Debug.Log(newWeapon.Intellect.ToString ());
+	}
+
+	public void CreateWeapon() {
+		newWeapon = new BaseWeapon ();
+
+		// Assign name to the weapon.
+		newWeapon.ItemName = "W" + Random.Range (1, 101);
+
+		// Create a weapon description.
+		newWeapon.ItemDescription = "This is a new Weapon.";
+
+		// Set weapon ID.
+		newWeapon.ItemID = Random.Range (1, 101);
+
+		// Set weapon stats.
+		newWeapon.Stamina = Random.Range (1, 11);
+		newWeapon.Endurance = Random.Range (1, 11);
+		newWeapon.Strength = Random.Range (1, 11);
+		newWeapon.Intellect = Random.Range (1, 11);
+
+		// Set weapon type.
+		ChooseWeaponType ();
+
+		// Set spell effect ID.
+		newWeapon.SpellEffectID = Random.Range (1, 101);
+	}
+
+	private void ChooseWeaponType() {
+		int randomTypeID = Random.Range (1, 7);
+		if (randomTypeID == 1) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.SWORD;
+		} else if (randomTypeID == 2) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.STAFF;
+		} else if (randomTypeID == 3) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.DAGGER;
+		} else if (randomTypeID == 4) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.BOW;
+		} else if (randomTypeID == 5) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.SHIELD;
+		} else if (randomTypeID == 6) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.POLEARM;
+		}
+	}
+}
